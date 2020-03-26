81ec7099645119bed05d93256935c74b0a8aad2c
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2020-03-26 14:44:50 +0100

    Clean all the volumes mounted by GH Actions

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,18 @@
 
 set -e # fail on error
 
-# Credits https://unix.stackexchange.com/a/77313
-rm -rf ..?* .[!.]* *
+function clean {
+  WORKDIR=$1
+  echo "Cleaning $WORKDIR"
+  cd "$WORKDIR"
+  rm -rf ..?* .[!.]* *  # Credits https://unix.stackexchange.com/a/77313
+}
+
+clean "$(pwd)"
+clean "/__w"
+clean "/__e"
+clean "/__w/_temp"
+clean "/__w/_actions"
+clean "/__w/_tool"
+clean "/github/home"
+clean "/github/workflow"
