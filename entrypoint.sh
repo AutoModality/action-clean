21ebe9764f9578c2b1a6d0614536bc4e529e6c1e
Aaron Roller <aaron.roller@automodality.com>
2020-04-26 10:19:33 -0700

    removing hidden files too

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,8 @@
 
 set -e # fail on error
 
-rm -rf *
+# include hidden files 
+# https://askubuntu.com/questions/740805/how-can-i-remove-all-files-from-current-directory-using-terminal
+shopt -s dotglob
+rm -r *
 
